b493703d8f7ae70dee5eb8f711ad065313fdd258
Raphael Macedo <raphaa.macedo@hotmail.com>
2021-04-20 00:26:50 -0300

    primeiro commit em javascript

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+//var a = true;
+//var b = false;
+
+//var c = a && b;
+
+//console.log(c);
+
+var idade = 59 ;
+var menor10 = idade <=  10;
+var maior65= idade >= 65;
+var gratuidade = menor10 || maior65 ;
+console.log("idade", idade);
+console.log("Menor que dez", menor10);
+console.log("Maior que sessenta e cinco", maior65);
+console.log("Tem direito a Gratuidade", gratuidade);
